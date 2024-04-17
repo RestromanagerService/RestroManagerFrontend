92825f1b19872376d92ab14f4a27a423906f5f46
AndresFSerranoPC <andresfserrano1@gmail.com>
2024-04-16 22:16:53 -0500

    Adding project initial project structure and first route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { LoginComponent } from './components/ui/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
