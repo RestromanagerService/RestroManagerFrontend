2d8422eb8091c11e60055941f30485b1ca16c3f9
AndresFSerranoPC <andresfserrano1@gmail.com>
2024-04-19 20:24:22 -0500

    end of header

---
+++ src/app/components/shared/navbar/navbar-link/navbar-link.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-link',
+  templateUrl: './navbar-link.component.html',
+  styleUrl: './navbar-link.component.css'
+})
+export class NavbarLinkComponent {
+  @Input() linkRoute!: string;
+  @Input() linkText!: string;
+}
