b4b24ee9a07ba3a32b3520373c0424dccf2840b6
oskarguzmant <oskarguzmant@gmail.com>
2024-05-17 16:57:02 -0500

    setting JSON and putting routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/ui/login/login.component';
-import { RegisterComponent } from './components/ui/register/register.component';
 import { CategoriesComponent } from './components/ui/categories/categories.component';
 import { EditCategoriesComponent } from './components/ui/categories/edit-categories/edit-categories.component';
 import { CreateCategoriesComponent } from './components/ui/categories/create-categories/create-categories.component';
@@ -15,13 +13,29 @@ import { RecipesComponent } from './components/ui/recipes/recipes.component';
 import { RecipesDetailsComponent } from './components/ui/recipes/recipes-details/recipes-details.component';
 import { TypeExpenseComponent } from './components/ui/type-expense/type-expense.component';
 import { ExpenseComponent } from './components/ui/expense/expense.component';
+import { RegisterUserComponent } from './components/ui/user/register-user/register-user.component';
+import { ConfirmEmailComponent } from './components/ui/user/confirm-email/confirm-email.component';
+
+import { EditUserComponent } from './components/ui/user/edit-user/edit-user.component';
+import { WelcomeComponent } from './components/ui/welcome/welcome.component';
+import { ResetPasswordComponent } from './components/ui/user/reset-password/reset-password.component';
+import { UserGuard } from './security/guards/user.guard';
+import { AdminGuard } from './security/guards/admin.guard';
+import { GuestGuard } from './security/guards/guest.guard';
+import { CountriesComponent } from './components/ui/countries/countries.component';
+import { StatesByCountryComponent } from './components/ui/countries/states-by-country/states-by-country.component';
+import { CitiesByStateComponent } from './components/ui/countries/cities-by-state/cities-by-state.component';
+import { AddCityComponent } from './components/ui/countries/add-city/add-city.component';
+import { EditCityComponent } from './components/ui/countries/edit-city/edit-city.component';
+import { ChangePasswordComponent } from './components/ui/user/change-password/change-password.component';
+import { FoodsDetailsComponent } from './components/ui/recipes/foods-details/foods-details.component';
+import { LoginComponent } from './components/ui/user/login/login.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register',component: RegisterComponent},
-  { path: 'categories',component: CategoriesComponent},
-  { path: 'categories/edit/:id',component: EditCategoriesComponent},
-  { path: 'categories/create',component: CreateCategoriesComponent},
+  { path: '',component: WelcomeComponent},
+  { path: 'categories',component: CategoriesComponent, canActivate:[AdminGuard]},
+  { path: 'categories/edit/:id',component: EditCategoriesComponent,canActivate:[AdminGuard]},
+  { path: 'categories/create',component: CreateCategoriesComponent,canActivate:[AdminGuard]},
   { path: 'stockCommercialProducts',component: StockcommercialproductsComponent},
   { path: 'stockCommercialProducts/edit/:id',component: EditstockcommercialproductsComponent},
   { path: 'stockCommercialProducts/create',component: CreatetockcommercialproductsComponent},
@@ -30,8 +44,21 @@ const routes: Routes = [
   { path: 'stockRawMaterials/create',component: CreateStockRawMaterialsComponent},
   { path: 'recipes',component: RecipesComponent},
   { path: 'recipes/details/:id',component: RecipesDetailsComponent},
+  { path: 'recipe/:idRecipe/food/rawMaterials/:id',component: FoodsDetailsComponent},
   { path: 'typeExpenses',component:TypeExpenseComponent},
-  { path: 'expenses',component:ExpenseComponent}
+  { path: 'expenses',component:ExpenseComponent},
+  { path: 'register',component: RegisterUserComponent},
+  { path: 'user/confirmEmail',component: ConfirmEmailComponent},
+  { path: 'user/edit',component: EditUserComponent},
+  { path: 'user/resetPassword',component: ResetPasswordComponent},
+  { path: 'user/changePassword', component:ChangePasswordComponent},
+  { path: 'countries', component:CountriesComponent},
+  { path: 'country/states/:id', component:StatesByCountryComponent},
+  { path: 'state/:idCountry/cities/:idState', component:CitiesByStateComponent},
+  { path: 'cities/:idCountry/create/:id', component:AddCityComponent},
+  { path: 'city/edit/:idCountry/:idState/:idCity', component:EditCityComponent},
+
+
 ];
 
 @NgModule({
